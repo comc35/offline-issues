7c836c593b98cb23e31dced20b9258a93154553e
comc35 <comc35@gmail.com>
2020-05-10 19:24:44 +0900

    Update script

--- run_script.sh
+++ run_script.sh
@@ -1,12 +1,14 @@
 list=$(ls md)
 TO_EXPORT=md_filtered
 mkdir -p $TO_EXPORT
-
+echo "#" List > $TO_EXPORT/list.md
 for i in $list ; do
 echo $i
-ISSUE_TITLE=$(cat md/$i | tail -n +5  | head -1)
-TITLE_REPO=$(echo $ISSUE_TITLE | sed -e "s/# //" | tr '[:upper:]' '[:lower:]' | sed -e 's/ /_/g' | sed -e 's/-/_/g'| sed -e 's/://g' | sed -e "s/\r//" )
+ISSUE_TITLE=$(cat md/$i | tail -n +5  | head -1 | sed -e "s/# //"  | sed -e "s/\r//"  )
+TITLE_REPO=$(echo $ISSUE_TITLE | tr '[:upper:]' '[:lower:]' | sed -e 's/ /_/g' | sed -e 's/-/_/g'| sed -e 's/://g' )
 echo $TITLE_REPO
 mkdir -p $TO_EXPORT/$TITLE_REPO
 cat md/$i | tail -n +5 | head -n -3  | sed -e "s/\r//" | sed '/^\!\[image\](/ s/$/\n/' | sed '/^\!\[image\](/s/^/\n/'  > $TO_EXPORT/$TITLE_REPO/readme.md
+
+echo "- ["$ISSUE_TITLE"]("papers/$TITLE_REPO/readme.md")" >> $TO_EXPORT/list.md
 done
